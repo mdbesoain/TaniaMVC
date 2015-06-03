d504bbb623d3ea744f0a06ed41200c348d53f854
juagonzalez <juagonzalez09@alumnos.utalca.cl>
2015-06-03 19:55:26 -0300

    Disciplina

---
+++ TaniaMVC/Controllers/DisciplinaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaniaMVC.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        //
+        // GET: /Disciplina/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
